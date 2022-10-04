7ab78109839dec340ec68b20b91fc1ed8848b733
Jintao Zhu <zhu.jintao@gm.com>
2022-10-04 17:41:41 +0800

    Parcel: fix memory leak
    Relying on finalize method to call destroy method will result in memory
    leak because there is no guarantee when or whether finalize will be
    executed.
    
    Solution: let recycle method call destroy method.
    
    Change-Id: I1db38e8ae89f06cc2fd460a5c9b5d18d434438e3
    Signed-off-by: Jintao Zhu <zhu.jintao@gm.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/os/Parcel.java
+++ core/java/android/os/Parcel.java
@@ -560,6 +560,7 @@ public final class Parcel {
                     mPoolNext = sOwnedPool;
                     sOwnedPool = this;
                     sOwnedPoolSize++;
+                    return;
                 }
             }
         } else {
@@ -569,9 +570,12 @@ public final class Parcel {
                     mPoolNext = sHolderPool;
                     sHolderPool = this;
                     sHolderPoolSize++;
+                    return;
                 }
             }
         }
+
+        destroy();
     }
 
     /**
