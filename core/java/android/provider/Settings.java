1936e1431f69cf59511ea256d9ae631cc4c9da8d
SamarV-121 <samarvispute121@pm.me>
2023-01-10 22:17:04 -0600

    base: Add settings to change QS tile shape [1/2]
    Change-Id: Ia8919fa755e5c865fc14cbeb9f4312e83cdbb750

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12461,10 +12461,16 @@ public final class Settings {
 
         /**
          * Whether to show an overlay in the bottom corner of the screen on copying stuff
-         * into the clipboard.
          * @hide
          */
         public static final String SHOW_CLIPBOARD_OVERLAY = "show_clipboard_overlay";
+
+        /**
+         * Quicksettings tile shape
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
 
         /**
          * Whether to ask unlock for using qs
